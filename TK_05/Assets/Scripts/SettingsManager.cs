a55cf8e499740b2c5680464705b6b3348a96209a
Proboszcz777 <watroba.grzegorz@gmail.com>
2019-12-09 11:07:47 +0100

    ECS Examples

---
+++ TK_05/Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public enum MobState
+{
+    ToTarget,
+    Throw,
+    FromTarget
+}
+
+public static class SettingsManager
+{
+    public static readonly float TargetSpeed = 100.0f;
+    public static readonly float ShootingRange = 50.0f;
+    public static readonly Vector3 ThrowingPoint = new Vector3(0.8f, 0.8f, 0);
+    public static readonly Vector3 ThrowingRotation = new Vector3(-45f, 0, 0);
+}
